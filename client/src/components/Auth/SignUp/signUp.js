8a077dea4db4aeb8a772b75dba1165891d93b2d6
FalconRam <ramvignesh974@gamil.com>
2022-12-13 12:46:30 +0530

    ++ Signin Functionaliy with Frontend validation

--- client/src/components/Auth/SignUp/signUp.js
+++ client/src/components/Auth/SignUp/signUp.js
@@ -1,14 +1,202 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { signUp } from "../../../actions/auth";
+import { findSigninFormErrors } from "../../../errorHandling/authFormEH";
 
 import "./signUp.css";
 
 const SignUp = ({ isLogin, setIsLogin }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [errors, setErrors] = useState({});
+  const [showFormError, setShowFormError] = useState(false);
+  const [signInData, setsignInData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    city: "",
+    country: "",
+  });
+
+  let clearForm = () => {
+    setsignInData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      city: "",
+      country: "",
+    });
+  };
+
+  const setField = (field, value) => {
+    setsignInData({
+      ...signInData,
+      [field]: value,
+    });
+    // Check and see if errors exist, and remove them from the error object:
+    if (!!errors[field])
+      setErrors({
+        ...errors,
+        [field]: null,
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(signInData);
+    const newErrors = findSigninFormErrors(signInData);
+    // Conditional logic:
+    if (Object.keys(newErrors).length > 0) {
+      // We got errors!
+      setErrors(newErrors);
+      setShowFormError(true);
+    } else {
+      // No errors!
+      // step : 1 - dispatching the action ; check step to in actions/auth
+      dispatch(signUp(signInData, history));
+      clearForm();
+    }
+  };
+
+  const switchMode = () => {
+    clearForm();
+    setIsLogin(!isLogin);
+  };
   return (
     <>
-      <div>
-        <button type="submit" class="btn btn-primary">
-          SignUp
-        </button>
+      <div className="col-sm-10 col-md-8 col-lg-5 mx-auto py-4 p-3 border rounded border-primary">
+        <p className="h3 text-center signIcon fw-bold">
+          <i className="bi bi-box-arrow-in-right me-2" />
+          SignIn
+        </p>
+        <form className="">
+          <div className="row g-2">
+            <div className="col-12 col-md-6">
+              <label htmlFor="exampleInputFirstName1" className="form-label">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                aria-label="First name"
+                onChange={(e) => setField("firstName", e.target.value)}
+                required
+              ></input>
+              {showFormError && <p className="formError">{errors.firstName}</p>}
+            </div>
+            <div className="col-12 col-md-6">
+              <label htmlFor="exampleInputLastName1" className="form-label">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+                aria-label="Last name"
+                onChange={(e) => setField("lastName", e.target.value)}
+                required
+              ></input>
+              {showFormError && <p className="formError">{errors.lastName}</p>}
+            </div>
+            <div className="col-12 col-md-62">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                aria-label="Email"
+                onChange={(e) => setField("email", e.target.value)}
+                required
+              ></input>
+              {showFormError && <p className="formError">{errors.email}</p>}
+            </div>
+            <div className="col-12 col-md-6">
+              <label htmlFor="exampleInputCity1" className="form-label">
+                City
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="City"
+                aria-label="City"
+                onChange={(e) => setField("city", e.target.value)}
+                required
+              ></input>
+              {showFormError && <p className="formError">{errors.city}</p>}
+            </div>
+            <div className="col-12 col-md-6">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Country
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Country"
+                aria-label="Country"
+                onChange={(e) => setField("country", e.target.value)}
+                required
+              ></input>
+              {showFormError && <p className="formError">{errors.country}</p>}
+            </div>
+            <div className="col-12 col-md-6">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                aria-label="Password"
+                onChange={(e) => setField("password", e.target.value)}
+                required
+              ></input>
+              {showFormError && <p className="formError">{errors.password}</p>}
+            </div>
+            <div className="col-12 col-md-6">
+              <label
+                htmlFor="exampleInputConfirmPassword1"
+                className="form-label"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Confirm Password"
+                aria-label="Confirm Password"
+                onChange={(e) => setField("confirmPassword", e.target.value)}
+                required
+              ></input>
+              {showFormError && (
+                <p className="formError">{errors.confirmPassword}</p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary d-grid col-sm-12 col-md-5 col-lg-4 mx-auto"
+              onClick={handleSubmit}
+            >
+              Sigin
+            </button>
+            <div className="d-flex flex-column flex-md-row justify-content-center ">
+              <button onClick={switchMode} className="p-0 mt-3 switchButton">
+                {isLogin
+                  ? "Don't have an account?"
+                  : "Already have an account?"}
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </>
   );
--- client/src/errorHandling/authFormEH.js
+++ client/src/errorHandling/authFormEH.js
@@ -14,3 +14,52 @@ export const findLoginFormErrors = (loginCred) => {
 
   return newErrors;
 };
+
+// Signin Error Handling
+export const findSigninFormErrors = (signInData) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    city,
+    country,
+    password,
+    confirmPassword,
+  } = signInData;
+  const newErrors = {};
+  const regex = /\S+@\S+\.\S+/;
+
+  // Name errors
+  if (!firstName || firstName === "")
+    newErrors.firstName = "Please enter your First Name.";
+  if (!lastName || lastName === "")
+    newErrors.lastName = "Please enter your Last Name.";
+
+  // Email errors
+  if (!email || email === "") newErrors.email = "Please enter your Email.";
+  else if (!regex.test(email)) newErrors.email = "Email address is invalid";
+
+  // City errors
+  if (!city || city === "") newErrors.city = "Please enter your City.";
+
+  // Country errors
+  if (!country || country === "")
+    newErrors.country = "Please enter your Country.";
+
+  // Password errors
+  if (!password || password === "")
+    newErrors.password = "Please enter your Password.";
+  else if (password.length <= 5) newErrors.password = "Password is too Short!";
+
+  // Confirm Password errors
+  if (!confirmPassword || confirmPassword === "")
+    newErrors.confirmPassword = "Please confirm your Password.";
+  else if (
+    password.length &&
+    confirmPassword.length > 5 &&
+    confirmPassword !== password
+  )
+    newErrors.confirmPassword = "Password is not Matching!";
+
+  return newErrors;
+};
