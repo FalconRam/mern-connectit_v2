bb71d93cd40b7a6f4bdbb4961cd8a4d6d3af0c4f
FalconRam <ramvignesh974@gamil.com>
2022-12-16 23:25:06 +0530

    Left Section Layout Completed with static data

--- client/src/components/Home/LeftSection/leftSection.js
+++ client/src/components/Home/LeftSection/leftSection.js
@@ -1,8 +1,28 @@
 import React from "react";
 
+import { useHistory } from "react-router-dom";
+
 import "./leftSection.css";
 
 const LeftSection = () => {
+  const history = useHistory();
+
+  const handleFollowFollows = () => {
+    history.push("/");
+  };
+
+  const handleProfile = () => {
+    history.push("/");
+  };
+
+  const handleSettings = () => {
+    history.push("/");
+  };
+
+  const handleSavedItems = () => {
+    history.push("/");
+  };
+
   return (
     <>
       <div className="col-sm-12 col-md-3">
@@ -18,26 +38,54 @@ const LeftSection = () => {
               className="img-thumbnail rounded-circle profilePic"
               alt="Profile Picture"
             ></img>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+            <div className="card-body pt-2 profileUser" onClick={handleProfile}>
+              <h6 className="card-title text-center text-primary">
+                Ram Vignesh M
+              </h6>
+              <p className="card-text text-center text-muted headline">
+                Javascript | Typescript | React | NodeJs | Bootstrap | Azure -
+                Software Engineer at Capgemini
               </p>
             </div>
             <ul className="list-group list-group-flush">
-              <li className="list-group-item">An item</li>
-              <li className="list-group-item">A second item</li>
-              <li className="list-group-item">A third item</li>
+              <li className="list-group-item liLeft">
+                <div
+                  className="text-center d-flex flex-row justify-content-center text-muted followFollows"
+                  onClick={handleFollowFollows}
+                >
+                  <span className="card-link followCount">
+                    <p className="mb-0 followCount">5</p>
+                    Following
+                  </span>
+                  <span className="card-link followCount">
+                    <p className="mb-0 followCount">5</p>
+                    Followers
+                  </span>
+                </div>
+              </li>
+              <li className="list-group-item text-muted">
+                <div className="text-center">
+                  <span
+                    className="card-link profileCardLinks"
+                    onClick={handleProfile}
+                  >
+                    My Profile
+                  </span>
+                  <span
+                    className="card-link profileCardLinks"
+                    onClick={handleSettings}
+                  >
+                    Settings
+                  </span>
+                </div>
+              </li>
+
+              <li className="list-group-item text-center text-muted savedItems liLeft">
+                <div onClick={handleSavedItems}>
+                  <i class="bi bi-bookmark-fill"></i> My items
+                </div>
+              </li>
             </ul>
-            <div className="card-body">
-              <a href="#" className="card-link">
-                Card link
-              </a>
-              <a href="#" className="card-link">
-                Another link
-              </a>
-            </div>
           </div>
         </div>
       </div>
---
+++ client/src/components/Posts/posts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./posts.css"
+
+const Posts = () => {
+  return (
+    <div>Posts</div>
+  )
+}
+
+export default Posts
