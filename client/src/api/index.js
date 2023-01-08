ac94b7bd16e9f78a7b58ba8c1d38b9a5ecf4e32c
FalconRam <ramvignesh974@gamil.com>
2023-01-08 10:29:37 +0530

    renderr deploy fix 3 - baseURL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://connectit.onrender.com" });
 
 // const url = "http://localhost:5000/posts";
 
