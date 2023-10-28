5976f9135d92e07c48e36b4e14e04dc81f77712c
Ramv Vignesh M <ramvignesh974@gmail.com>
2023-10-29 00:11:41 +0530

    Likes Comp moved to sahred folder

---
+++ client/src/components/Shared/Likes/likes.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+import "./likes.css";
+
+const Likes = ({
+  isComment,
+  isLiked,
+  likes,
+  likeFrom,
+  handlePostLike,
+  handleCommentLike,
+  handleReplyLike,
+}) => {
+  let type = {
+    postCard: "postCard",
+    commentModal: "commentModal",
+    commentReply: "commentReply",
+  };
+  const handleLike = () => {
+    switch (likeFrom) {
+      case type.postCard:
+        handlePostLike();
+        break;
+      case type.commentModal:
+        handleCommentLike();
+        break;
+      case type.commentReply:
+        console.log("Dipatching Reply like");
+        handleReplyLike();
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <>
+      {!isComment ? (
+        <>
+          {isLiked ? (
+            // Red Heart
+            <div className="d-flex align-items-center">
+              <i
+                className="bi bi-heart-fill likeIcon likeIcon-danger"
+                onClick={handleLike}
+              />
+              <p className="p-like">
+                &nbsp;
+                {likes > 1 ? `${likes} Likes` : `${likes} Like`}
+              </p>
+            </div>
+          ) : (
+            // Heart Outline
+            <div className="d-flex align-items-center">
+              <i className="bi bi-heart likeIcon" onClick={handleLike} />
+              &nbsp;
+              <p className="p-like">
+                &nbsp;
+                {likes > 1 ? `${likes} Likes` : `Like`}
+              </p>
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          {isLiked ? (
+            // Red Heart
+            <div className="d-flex align-items-center">
+              <i
+                className="bi bi-heart-fill likeIcon likeIcon-danger"
+                onClick={handleLike}
+              />
+            </div>
+          ) : (
+            // Heart Outline
+            <div className="d-flex align-items-center">
+              <i className="bi bi-heart likeIcon" onClick={handleLike} />
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Likes;
