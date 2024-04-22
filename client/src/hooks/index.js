e6042affcfa3b86b4cc93b8b97b966fd11c8a2cf
FalconRam <ramvignesh974@gmail.com>
2024-04-22 21:19:56 +0400

    fix: resolved merge conflict

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,5 +1,33 @@
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-export function useQuery() {
+import { logIn } from "../actions/auth";
+
+const useQuery = () => {
   return [new URLSearchParams(useLocation().search)];
-}
+};
+
+const useTestAccountLogin = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [isLoadingWithTest, setIsLoadingWithTest] = useState(false);
+
+  const handleTestAccountLogin = async () => {
+    setIsLoadingWithTest(true);
+    await dispatch(
+      logIn(
+        {
+          email: process.env.REACT_APP_TEST_EMAIL,
+          password: process.env.REACT_APP_TEST_PASSWORD,
+        },
+        history
+      )
+    );
+    setIsLoadingWithTest(false);
+  };
+  return [isLoadingWithTest, handleTestAccountLogin];
+};
+
+export { useTestAccountLogin, useQuery };
